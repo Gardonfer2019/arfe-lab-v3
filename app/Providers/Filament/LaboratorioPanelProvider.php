f53ebe9ca6b83355e1a9b802c99d3c17879c2972
Gustavo <hackeye99.dc@gmail.com>
2024-10-03 09:45:27 -0600

    Agregar icono

--- app/Providers/Filament/LaboratorioPanelProvider.php
+++ app/Providers/Filament/LaboratorioPanelProvider.php
@@ -31,6 +31,7 @@ class LaboratorioPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Indigo,
             ])
+            ->favicon(asset('images/microscope.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
