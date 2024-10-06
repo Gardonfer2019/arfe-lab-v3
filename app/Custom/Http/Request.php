69906b30ea4c54f3d28b000364fa2006a292a371
Gustavo <hackeye99.dc@gmail.com>
2024-10-06 02:36:43 -0600

    Corregir bug

--- app/Custom/Http/Request.php
+++ app/Custom/Http/Request.php
@@ -1,20 +1,24 @@
 <?php 
 namespace App\Custom\Http;
-use \Illuminate\Http\Request as Base;
 
-class Request extends Base {
+use Illuminate\Http\Request as Base;
 
-    public function isSecure() {
+class Request extends Base
+{
+    public function isSecure(): bool
+    {
         $isSecure = parent::isSecure();
         if ($isSecure) {
             return true;
         }
         if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
             return true;
-        } else if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            return true;
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
             return true;
         }
+
         return false;
     }
-
 }
